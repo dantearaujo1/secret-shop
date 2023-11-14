77d28d277f95d40fc34c9acfdaffccd244c3ee2b
Dante de Araujo <danteeng@hotmail.com>
2023-11-13 22:46:47 -0300

    Adicionando Lombok como dependencia do projeto

--- backend/src/main/java/com/smd/umake/entities/Product.java
+++ backend/src/main/java/com/smd/umake/entities/Product.java
@@ -15,6 +15,17 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.With;
+
+@Getter
+@Setter
+@With
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity(name="Product")
 @Table(name="product")
 public class Product{
