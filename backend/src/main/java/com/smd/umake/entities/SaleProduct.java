d9e60c4a10e462920e850491308aef56cb9dedf9
Dante de Araujo <danteeng@hotmail.com>
2023-11-14 23:30:34 -0300

    Corrigindo erro para chaves que são classes dentro das entidades do banco de dados

--- backend/src/main/java/com/smd/umake/entities/SaleProduct.java
+++ backend/src/main/java/com/smd/umake/entities/SaleProduct.java
@@ -1,10 +1,5 @@
 package com.smd.umake.entities;
 
-import java.io.Serializable;
-import java.util.UUID;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Embeddable;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
@@ -12,14 +7,6 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MapsId;
 import jakarta.persistence.Table;
 
-@Embeddable
-class SaleProductKey implements Serializable{
-  @Column(name="id_sale")
-  UUID sale_id;
-
-  @Column(name="id_product")
-  UUID product_id;
-}
 
 @Entity(name="SaleProduct")
 @Table(name="sale_product")
--- backend/src/main/java/com/smd/umake/entities/Stock.java
+++ backend/src/main/java/com/smd/umake/entities/Stock.java
@@ -1,10 +1,5 @@
 package com.smd.umake.entities;
 
-import java.io.Serializable;
-import java.util.UUID;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Embeddable;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
@@ -12,14 +7,6 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MapsId;
 import jakarta.persistence.Table;
 
-@Embeddable
-class StockKey implements Serializable{
-  @Column(name="id_product")
-  UUID product_id;
-
-  @Column(name="id_branch")
-  UUID branch_id;
-}
 
 @Entity(name="Stock")
 @Table(name="stock")
