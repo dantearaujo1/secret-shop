a74c74fb45fbd6fe3648d8ca9fdb4509cafa93fd
Dante de Araujo <danteeng@hotmail.com>
2023-11-15 18:28:07 -0300

    Implementação das rotas de produtos e categorias juntamente com a criação de um DTO para ficar no layer da API

--- backend/src/main/java/com/smd/umake/controllers/ProductController.java
+++ backend/src/main/java/com/smd/umake/controllers/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.smd.umake.dtos.ProductDTO;
 import com.smd.umake.entities.Product;
 import com.smd.umake.entities.ProductCat;
 import com.smd.umake.services.ProductCategoryService;
@@ -29,40 +31,91 @@ public class ProductController{
   private ProductCategoryService categoryService;
 
   @GetMapping("/{id}")
-  public Product getProductById(@PathVariable UUID id) throws Exception {
-    return productService.getProductById(id);
+  public ResponseEntity<Product> getProductById(@PathVariable UUID id) throws Exception {
+    Product p = productService.getProductById(id);
+    if (p == null){
+      throw new Exception();
+    } else {
+      return new ResponseEntity<> (p,HttpStatus.OK);
+    }
   }
   @GetMapping("")
-  public Product getProductById(@RequestParam("name") String name) throws Exception {
-    return productService.getProductByName(name);
+  public ResponseEntity<Product> getProductById(@RequestParam("name") String name) throws Exception {
+    Product p = productService.getProductByName(name);
+    if (p == null){
+      throw new Exception();
+    } else {
+      return new ResponseEntity<> (p,HttpStatus.OK);
+    }
+  }
+  @GetMapping("/")
+  public ResponseEntity<List<Product>> getProducts() throws Exception {
+    List<Product> p = productService.getProducts();
+    if (p == null){
+      throw new Exception();
+    } else {
+      return new ResponseEntity<> (p,HttpStatus.OK);
+    }
   }
   @DeleteMapping("/{id}")
-  public String deleteProductById(@PathVariable UUID id) throws Exception {
+  public ResponseEntity<String> deleteProductById(@PathVariable UUID id) throws Exception {
     String result = productService.deleteProduct(id);
-    return result;
+    return new ResponseEntity<String>(result,HttpStatus.OK);
+  }
+  @PostMapping("/")
+  public ResponseEntity<Product> addProduct(@RequestBody ProductDTO  newProduct) throws Exception{
+    Product prod = productService.createProduct(newProduct);
+    if (prod == null){
+      throw new Exception();
+    } else {
+      return new ResponseEntity<Product>(prod, HttpStatus.CREATED);
+    }
 
+  }
+  @PatchMapping("/{id}")
+  public ResponseEntity<Product> updatePartialProductById(@PathVariable UUID id, @RequestBody ProductDTO updatedData) throws Exception {
+    Product prod = productService.updatePartialProduct(id, updatedData);
+    if (prod == null){
+      throw new Exception();
+    } else {
+      return new ResponseEntity<Product>(prod, HttpStatus.CREATED);
+    }
+  }
+  @GetMapping("/category")
+  public ResponseEntity<List<ProductCat>> getCategories() throws Exception {
+    List<ProductCat> c = categoryService.getCategories();
+    if (c == null){
+      throw new Exception();
+    } else {
+      return new ResponseEntity<> (c,HttpStatus.OK);
+    }
+  }
+  @GetMapping(path = "/category", params = "name")
+  public ResponseEntity<ProductCat> getCategoryByName(@RequestParam("name") String name) throws Exception {
+    ProductCat c = categoryService.getCategoryByName(name);
+    if (c == null){
+      throw new Exception();
+    } else {
+      return new ResponseEntity<> (c,HttpStatus.OK);
+    }
+  }
+  @GetMapping(path = "/category/{id}")
+  public ResponseEntity<ProductCat> getCategoryByName(@PathVariable("id") UUID id) throws Exception {
+    ProductCat c = categoryService.getCategoryById(id);
+    if (c == null){
+      throw new Exception();
+    } else {
+      return new ResponseEntity<> (c,HttpStatus.OK);
+    }
   }
   @DeleteMapping("category/{id}")
   public String deleteCategoryById(@PathVariable UUID id) throws Exception {
+    // TODO: Implementar checagem de nulo
     String result = categoryService.deleteCategory(id);
     return result;
   }
-  @GetMapping("/")
-  public List<Product> getProducts() throws Exception {
-    return productService.getProducts();
-  }
-
-  @GetMapping("/category")
-  public List<ProductCat> getCategories() throws Exception {
-    return categoryService.getCategories();
-  }
-  @GetMapping(path = "/category", params = "name")
-  public ProductCat getCategoryByName(@RequestParam("name") String name) throws Exception {
-    return categoryService.getCategoryByName(name);
-  }
-
   @PostMapping("/category")
-  public ResponseEntity<ProductCat> addProduct(@RequestBody ProductCat  newCategory) throws Exception{
+  public ResponseEntity<ProductCat> addCategory(@RequestBody ProductCat  newCategory) throws Exception{
     ProductCat cat = categoryService.createCategory(newCategory);
     if (cat == null){
       throw new Exception();
@@ -72,15 +125,5 @@ public class ProductController{
 
   }
 
-  @PostMapping("/")
-  public ResponseEntity<Product> addProduct(@RequestBody Product  newProduct) throws Exception{
-    Product prod = productService.createProduct(newProduct);
-    if (prod == null){
-      throw new Exception();
-    } else {
-      return new ResponseEntity<Product>(prod, HttpStatus.CREATED);
-    }
-
-  }
 
 }
--- backend/src/main/java/com/smd/umake/services/ProductService.java
+++ backend/src/main/java/com/smd/umake/services/ProductService.java
@@ -7,7 +7,10 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.smd.umake.dtos.ProductDTO;
 import com.smd.umake.entities.Product;
+import com.smd.umake.entities.ProductCat;
+import com.smd.umake.repositories.ProductCatRepository;
 import com.smd.umake.repositories.ProductRepository;
 
 @Service
@@ -15,6 +18,8 @@ public class ProductService{
 
   @Autowired
   private ProductRepository productRepository;
+  @Autowired
+  private ProductCatRepository categoryRepository;
 
   public Product getProductById(UUID id) throws Exception {
     Optional<Product> oProduct = productRepository.findById(id);
@@ -30,14 +35,45 @@ public class ProductService{
     return products;
   }
 
-  public Product createProduct(Product newProduct) throws Exception {
-    Product oProduct = productRepository.save(newProduct);
+  public Product createProduct(ProductDTO newProduct) throws Exception {
+    Product prod = new Product();
+    prod.setName(newProduct.getName());
+    prod.setDescription(newProduct.getDescription());
+    prod.setBrand(newProduct.getBrand());
+    prod.setPrice(newProduct.getPrice());
+
+    String categoryID = newProduct.getCategoryID();
+    if ( categoryID == null || (categoryID.isBlank()) ){
+      Product oProduct = productRepository.save(prod);
+      return oProduct;
+    }
+
+    UUID categoryUUID = UUID.fromString(categoryID);
+    Optional<ProductCat> category = categoryRepository.findById(categoryUUID);
+    if (category.isPresent()){
+      prod.setCategory(category.get());
+    }
+
+    Product oProduct = productRepository.save(prod);
     return oProduct;
   }
 
-  public Product updateProduct(Product newProduct) throws Exception {
-    Product oProduct = productRepository.save(newProduct);
-    return oProduct;
+  public Product updateProduct(UUID id, ProductDTO newProduct) throws Exception {
+    Optional<Product> oProduct = productRepository.findById(id);
+    // TODO: Implementar o update total
+    if (oProduct.isPresent()){
+      return oProduct.get();
+    }
+    return null;
+  }
+
+  public Product updatePartialProduct(UUID id, ProductDTO newProduct) throws Exception {
+    Optional<Product> oProduct = productRepository.findById(id);
+    // TODO: Implementar o update parcial
+    if (oProduct.isPresent()){
+      return oProduct.get();
+    }
+    return null;
   }
 
   public String deleteProduct(UUID id) throws Exception {
